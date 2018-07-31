038beb31394f2427480135cc40d176f81b45d237
Vince <vincentfiorilli@gmail.com>
2018-07-31 06:18:34 -0400

    some events

--- secondapp/src/components/Contact.js
+++ secondapp/src/components/Contact.js
@@ -3,14 +3,20 @@ import PropTypes from "prop-types";
 
 class Contact extends Component {
   render() {
-    const { name, email, phone } = this.props;
+    const { contact } = this.props;
 
     return (
       <div className="card card-body mb-3">
-        <h4>{this.props.name}</h4>
-        <ul class="list-group">
-          <li class="list-group-item">email: {this.props.email}</li>
-          <li class="list-group-item">phone: {phone}</li>
+        <h4>
+          {contact.name}
+          <i
+            onClick={() => console.log("hello")}
+            className="fas fa-sort-down"
+          />
+        </h4>
+        <ul className="list-group">
+          <li className="list-group-item">email: {contact.email}</li>
+          <li className="list-group-item">phone: {contact.phone}</li>
         </ul>
       </div>
     );
--- secondapp/src/components/Contacts.js
+++ secondapp/src/components/Contacts.js
@@ -2,31 +2,28 @@ import React, { Component } from "react";
 import Contact from "./Contact";
 
 class Contacts extends Component {
-  constructor() {
-    super();
-    this.state = {
-      contacts: [
-        {
-          id: 1,
-          name: "John Doe",
-          email: "jdoe@gmail.com",
-          phone: "555-555-5555"
-        },
-        {
-          id: 2,
-          name: "Karen Schmaron",
-          email: "kdogg@gmail.com",
-          phone: "666--666-666"
-        },
-        {
-          id: 3,
-          name: "Henry Hardass",
-          email: "Henryyyy@gmail.com",
-          phone: "111-111-1111"
-        }
-      ]
-    };
-  }
+  state = {
+    contacts: [
+      {
+        id: 1,
+        name: "John Doe",
+        email: "jdoe@gmail.com",
+        phone: "555-555-5555"
+      },
+      {
+        id: 2,
+        name: "Karen Schmaron",
+        email: "kdogg@gmail.com",
+        phone: "666--666-666"
+      },
+      {
+        id: 3,
+        name: "Henry Hardass",
+        email: "Henryyyy@gmail.com",
+        phone: "111-111-1111"
+      }
+    ]
+  };
 
   render() {
     //destructuring
@@ -35,12 +32,7 @@ class Contacts extends Component {
     return (
       <div>
         {contacts.map(contact => (
-          <Contact
-            key={contact.id}
-            name={contact.name}
-            email={contact.email}
-            phone={contact.phone}
-          />
+          <Contact key={contact.id} contact={contact} />
         ))}
       </div>
     );
